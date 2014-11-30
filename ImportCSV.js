27cbff14a88358a610438c8a7b9ac94979b6a203
TheFive <thefive.osm@gmail.com>
2014-11-30 12:06:58 +0100

    ddd

--- ImportCSV.js
+++ ImportCSV.js
@@ -1,6 +1,13 @@
 fs=require("fs");
 
 
+// Code for parsing simple CSV Files including ""
+// Code taken from: http://stackoverflow.com/questions/1293147/javascript-code-to-parse-csv-data
+// and partially modified
+
+// Input: string containing CSV Data
+// Output: Array of JSON Objects
+
 function parseCSV(str) {
     var arr = [];
     var quote = false;  // true means we're inside a quoted field
@@ -32,7 +39,7 @@ function parseCSV(str) {
     return arr;
 }
 
-
+// internal Function to duplicate an object
 function clone(obj) {
     if (null == obj || "object" != typeof obj) return obj;
     var copy = obj.constructor();
@@ -43,36 +50,46 @@ function clone(obj) {
 }
 
 
+// readCSV Function to import CSV to the Measure Database
+// Input: a MongoDB, a template JSON, CSV Filename, encoding 
 exports.readCSV = function (db,defJson,filename,encoding) {
+	
+	// Open the file and copy it to a string
+	// encoding is ignored yet
 	fs.readFile(filename, 'UTF8', function (err,data) {
 		if (typeof(db)=='undefined') {
-			console.log("Fehlerhafte Datenbank im ImportCSV");
+			console.log("Internal Error db undefined");
 			return;
 		}
-		else {
-			console.log("Type Of Data "+typeof(data));
-		}
+		
 		if (err) {
 			console.log(err);
 			return;
 		}
+		
+		// convert the content of the file to an JSON array
 		array = parseCSV(data);
+		
 		if(array.length<2) {
-			console.log("Emty File "+filename);
-			//console.log(array[0]);
+			// CSV File is empty, log an error
+			console.log("Empty File "+filename);
 			return;
 		}
 		else {
-		
+		    // just log the column and rows for debugging reasons
 			console.log("Number of Lines in CSV "+array.length);
 			console.log("Number of Columns in CSV"+array[0].length)
 		}
+		
+		// Quality Check, does all rows has the same count of columns ?
 		for (i=1;i<array.length;i++) {
 			if (array[0].length!=array[i].length) {
 				console.log("Invalid CSV File, Number of Columns differs "+filename +" Zeile "+i);
 				return
 			}
 		}
+		
+		// Copy measures into the DataCollection MongoDB
 		var collection = db.collection('DataCollection');
 		newData = [];
 		for (i=1;i<array.length;i++) {
@@ -80,12 +97,17 @@ exports.readCSV = function (db,defJson,filename,encoding) {
 			for (z=0;z<array[0].length;z++) {
 				key = array[0][z];
 				value = array[i][z];
+				
 				//console.log("---- "+i+" "+z);
 				//console.log(key +":"+value);
-				newData[i-1][key]=value;
+				if (typeof(defJSON)!='integer') {
+					//console.log(key + typeof(newData[value]));
+					newData[i-1][key]=value;
+				} else {
+					newData [i-1]=Integer.parseInt(value);
+				}
 			}
 		}
 		collection.insert(newData,{w:1},function (){});
-		
    	});
 }
--- server.js
+++ server.js
@@ -1,45 +1,196 @@
-var express = require('express');
-var app = express();
-
 //require node modules (see package.json)
+var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 
 
+// require own modules
+var importCSV=require('./importCSV');
+
+
+
+var app = express();
+
+
+
+// Reading the configuration file
+
+var fs, configurationFile;
+
+configurationFile = 'configuration.json';
+fs = require('fs');
+
+var configuration = JSON.parse(
+  fs.readFileSync(configurationFile)
+);
+
+// Log the information from the file
+console.log(configuration);
+
+
+
+var mongodb;
+
+var mongodbConnectStr ='mongodb://'
+                   + configuration.username + ':'
+                   + configuration.password + '@'
+                   + configuration.database;
+                   
+console.log("connect:"+mongodbConnectStr);
+
+MongoClient.connect(mongodbConnectStr, function(err, db) {
+  if (err) throw err;
+  mongodb = db;
+  console.log("Connected to Database mosmcount");
+  })
 
 
 
 
 app.use(function(req, res, next){
     console.log(req.url);
+    res.db = mongodb;
     next();
 });
+
+// Zeige eine Count Seite
 app.use('/count.html', function(req,res){
-
-
-MongoClient.connect('mongodb://127.0.0.1:27017/mosmcount', function(err, db) {
-  if (err) throw err;
-  console.log("Connected to Database mosmcount");
+    db = res.db;
   
     // Fetch the collection test
     var collection = db.collection('DataCollection');
     collection.count(function(err, count) {
           res.end("There are " + count + " records.");
-        });
-        
-       
-        
-        
-        
+        });  
+	})
 
+
+app.use('/import.html', function(req,res){
+    db = res.db;
+  
+    // Fetch the collection test
+    defJSON = { measure: "AddrWOStreet",  count: 0};
+    console.log("Count hat den typ"+typeof(defJSON["count"]));
+    importCSV.readCSV(db,  defJSON,"allLF-UTF8.csv");
+    res.end("File IMported Probably");
 	})
-	})
+
+
+// Zeige eine Tabelle Seite
+app.use('/table.html', function(req,res){
+	db = res.db;
+  
+    // Fetch the collection DataCollection
+    // To be Improved with Query or Aggregation Statments
+    var collection = db.collection('DataCollection');
+    
+    displayAllText =[{$match: { measure: "AddrWOStreet"}},
+    						 //{$project: { schluessel: "$schluessel",count: "$count",timestamp: "$timestamp"}},
+    						 {$group: { _id : "$_id" , 
+    						 		    count: {$sum: "$count"},
+    						 		    schluessel: {$first:"$schluessel"},
+    						 		    timestamp: {$last:"$timestamp"}}},
+    						 {$sort: { schluessel:1,timestamp:1}},
+    						 
+    						
+    						];
+    displayBundeslandText = [{$match: { measure: "AddrWOStreet"}},
+    						 {$project: { schluessel: { $substr: ["$schluessel",0,2]},
+    						 			  countintern: "$count",
+    						 			  timestamp: "$timestamp"}},
+    						 {$group: { _id: { schluessel: "$schluessel",timestamp:"$timestamp"},
+    						 		    count: {$sum: "$countintern" }}},
+    						 {$sort: { schluessel:1,timestamp:1}},
+    						 
+    						
+    						];
+    // Bitte Checken, Parameter geht noch nicht
+    var aggFunc=displayAllText;   						
+    if (req.param("all")=="NO"){
+    	aggFunc=displayBundeslandText;
+    } 
+    
+    // change the Collection to a HTML Table
+    collection.aggregate(	displayBundeslandText
+    
+    
+    						, (function(err, items) {
+    	// first copy hole table in a 2 dimensional JavaScript map
+    	// may be here is some performance potential :-)
+    	if (err) {
+    		res.end("error");
+    		console.log(err);
+    		return;
+    	}
+    	// Initialising JavaScript Map
+    	header = [];
+		firstColumn = [];
+		table =[]; 
+		
+		beforetext= "Table contains " + items.length+"  items";
+		
+		//iterate total result array
+		for (i=0;i < items.length;i++) {
+			
+		
+			measure = items[i];
+			console.dir(measure);
+			
+			//generate new Header or Rows and Cell if necessary	
+			if (header.indexOf(measure.timestamp)<0) {
+				header.push(measure.timestamp);
+			}
+			if (firstColumn.indexOf(measure.schluessel)<0) {
+				firstColumn.push(measure.schluessel);
+			}
+			if (!Array.isArray(table[measure.schluessel])) {
+				table[measure.schluessel]=[];
+			}
+			
+			table[measure.schluessel][measure.timestamp]=parseInt(measure.count);
+			//console.log(measure.schluessel+","+measure.timestamp+","+cell);
+			//console.log("-->"+table[measure.schluessel][measure.timestamp]);
+		}
+		tableheader = "<th>Regioschlüssel</th>";
+		for (i=0;i<header.length;i++) {
+			tableheader +="<th>"+header[i]+"</th>";
+		}
+		tableheader = "<tr>"+tableheader + "</tr>";
+		tablebody="";
+		for (i=0;i<firstColumn.length;i++)
+		{
+			schluessel = firstColumn[i];
+			var row = "<td>"+schluessel+"</td>";
+			for (z=0;z<header.length;z++) {
+				timestamp=header[z];
+				//console.log(schluessel+","+timestamp+"->"+table[schluessel][timestamp]);
+				var cell;
+				var content=table[schluessel][timestamp];
+				if (typeof(content) == "undefined") {
+					cell ="-";
+				} else {
+					cell = content;
+				}
+					
+				row += "<td>"+cell+"</td>";
+			}
+			row = "<tr>"+row+"</tr>";
+			tablebody += row;
+		}
+		tableheader += "</tr>";
+		tablebody +="</tr>"	;
+		tablebody += "";
+		text = "<html><body>"+beforetext+"<table border=\"1\">\n" + tableheader + tablebody + "</table></body></head>";
+		res.end(text);
+	}));
+	});
+	
  
 app.use('/', express.static(__dirname));
 app.get('/*', function(req, res) {
     res.status(404).sendFile(__dirname + '/error.html');
 });
-app.listen(64163);
+app.listen(configuration.serverport);
 
-console.log("server has started and is listening to http://127.0.0.1:64163");
+console.log("Server has started and is listening to localhost:"+configuration.serverport);
 	
