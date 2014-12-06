853a417c7021cc090c7a527f67d96408acc5e2d4
TheFive <thefive.osm@gmail.com>
2014-12-06 17:11:10 +0100

    configuration added

---
+++ configuration.js
@@ -0,0 +1,47 @@
+var fs, configurationFile;
+
+configurationFile = 'configuration.json';
+fs = require('fs');
+
+var configuration;
+
+// Log the information from the file
+console.log(configuration);
+
+
+
+var MongoClient = require('mongodb').MongoClient;
+var mongodb;
+
+
+
+
+exports.initialise = function() {
+	configuration = JSON.parse(fs.readFileSync(configurationFile));
+}
+
+
+
+  
+exports.getConfiguration = function() {
+	return configuration;
+}
+
+exports.getDB = function() {
+    if (typeof(mongodb)=='undefined') console.log("MongoDB Undefined");
+	return mongodb;
+}
+
+exports.getDBString = function() {
+   var mongodbConnectStr ='mongodb://'
+                   + configuration.username + ':'
+                   + configuration.password + '@'
+                   + configuration.database;
+    console.log("connect:"+mongodbConnectStr);
+    return mongodbConnectStr;        
+}
+
+exports.getServerPort = function() {
+	return configuration.serverport;
+}
+            
