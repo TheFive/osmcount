5a32157ac9287015f4a785f47887a50f1c83ed85
TheFive <thefive.osm@gmail.com>
2015-02-10 22:03:18 +0100

    Testing

--- test.js
+++ test.js
@@ -1,45 +1,29 @@
-var plotly = require('plotly')('thefive.osm','8c8akglymf');
-var debug    = require('debug')('test');
-  debug.data = require('debug')('test:data');
-  debug.entry = require('debug')('test:entry');
-var configuration = require('./configuration.js');
-var loadDataFromDB = require('./LoadDataFromDB.js');
-var loadOverpassData = require('./LoadOverpassData.js');
-var request = require('request');
-var ObjectID = require('mongodb').ObjectID;
+function cleanObject(obj) {
+	
+     console.log("Enterong clean Object");
+     for (k in obj) {
+     	console.log(k);
+     	if (typeof (obj[k]) =='object') {
+     	  cleanObject(obj[k]);
+     	}
+        console.log(k.indexOf('.'));
+        
+     	if (k.indexOf(".") >0 ) {
+     	  console.log( "delete "+k);
+     	    //console.log("Fount ." +k.search("."));
+     		//console.dir(data);
+     		
+     		delete obj[k]; 
+     		//console.log("After Delete");
+     		//console.dir(data);
+     	}
+     }
+   }
+   
+   
+   
+o = { tag :1, tag2:2, tag3:2, subtags: {"asdf.asdf":4,asdf:3}}
 
+cleanObject(o);
 
-var object=ObjectID("54bbda23a9fff29864d116b4");
-
-var async    = require('async');
-
-function readError(cb,result) {
-
-
-configuration.getDB().collection("DataCollection").find( 
-							{source:object,
-							$and: 
-                         [{measure: {$ne:"Apotheke"}},{measure:{$ne:"AddrWOStreet"}}]}).toArray( 
-								function(err,data) {
-  if (err) {
-    console.log("Error: "+JSON.stringify(err));
-    return;
-  }
-  console.log("Fehler gefunden: "+data.length);
-  var schluesselList = [];
-  for (i=0;i<data.length;i++) {
-  	d = data[i];
-  	schluesselList.push(d.schluessel);
-  }			
-  cb(null,schluesselList);					
-								
-});}
-
-async.auto( {db:configuration.initialiseDB,
-	         error:["db",readError]},
-	         function (cb,result) {
-	         	console.dir(result.error);
-	         	cb();
-	         }
-             
-             );
+console.dir(o);
