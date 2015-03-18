1e0011030072baa1d617498e34bff8e7bcc8b350
TheFive <thefive.osm@gmail.com>
2015-03-18 08:56:36 +0100

    extension of model

---
+++ test/DataTarget.test.js
@@ -0,0 +1,34 @@
+var pg=require('pg');
+var fs = require('fs');
+var path=require('path');
+var async = require('async');
+var should = require('should');
+
+var configuration = require('../configuration.js');
+var DataTarget = require('../model/DataTarget.js')
+
+
+describe('DataTarget', function() {
+  describe('import',function(bddone) {
+    beforeEach(function(bddone) {
+      async.series([
+        DataTarget.dropTable,
+        DataTarget.createTable
+      ],function(err) {
+        if (err) console.dir(err);
+        should.equal(null,err);
+        bddone();
+      });
+    });
+    it('should import data',function(bddone){
+      var filename = path.resolve(__dirname, "DataTarget.test.json");
+      //var filestring = fs.readFileSync(filename,{encoding:'UTF8'});
+      DataTarget.import(filename,function(err,data){
+        if (err) console.dir(err);
+        should.not.exist(err,null);
+        should.equal(data,"Datensätze: 2");
+        bddone();
+      });
+    });
+  });
+})
